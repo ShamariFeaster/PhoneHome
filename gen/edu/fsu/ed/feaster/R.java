f88162bbf7ff306fca0382568f4165aa45edfed8
Joshua Beizer <Josh.beizer@gmail.com>
2011-10-26 21:13:15 -0400

    added a ui screen but did NOT link, did not touch any code

--- gen/edu/fsu/ed/feaster/R.java
+++ gen/edu/fsu/ed/feaster/R.java
@@ -12,25 +12,38 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int phone=0x7f020001;
+        public static final int sad=0x7f020002;
     }
     public static final class id {
-        public static final int TextView01=0x7f070001;
-        public static final int button1=0x7f070007;
-        public static final int button2=0x7f070008;
-        public static final int editText1=0x7f070002;
-        public static final int linearLayout1=0x7f070000;
-        public static final int linearLayout2=0x7f070004;
-        public static final int message_text=0x7f070009;
-        public static final int message_to_finder_text=0x7f070006;
-        public static final int number_display=0x7f07000b;
-        public static final int radioGroup1=0x7f070005;
-        public static final int stop_timer_button=0x7f07000c;
-        public static final int textView1=0x7f070003;
-        public static final int timer_description=0x7f07000a;
+        public static final int Contact=0x7f070006;
+        public static final int LinearLayout1=0x7f070000;
+        public static final int TextView01=0x7f07000b;
+        public static final int button1=0x7f070011;
+        public static final int button2=0x7f070012;
+        public static final int call=0x7f070003;
+        public static final int editText1=0x7f07000c;
+        public static final int imageView1=0x7f070009;
+        public static final int linearLayout1=0x7f07000a;
+        public static final int linearLayout2=0x7f07000e;
+        public static final int logo=0x7f070005;
+        public static final int message_text=0x7f070013;
+        public static final int message_to_finder_text=0x7f070010;
+        public static final int number_display=0x7f070015;
+        public static final int phoneNumberDynamic=0x7f070008;
+        public static final int phoneText=0x7f070007;
+        public static final int radioGroup1=0x7f07000f;
+        public static final int sad=0x7f070004;
+        public static final int stop_ring=0x7f070002;
+        public static final int stop_timer_button=0x7f070016;
+        public static final int text=0x7f070001;
+        public static final int textView1=0x7f07000d;
+        public static final int timer_description=0x7f070014;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
-        public static final int stop_timer_layout=0x7f030001;
+        public static final int found=0x7f030000;
+        public static final int main=0x7f030001;
+        public static final int stop_timer_layout=0x7f030002;
     }
     public static final class raw {
         public static final int alarm=0x7f050000;
